eceda8dfec90cd3def7945f92ce6c7ea2cbade08
Olaf Alders <olaf@wundersolutions.com>
2024-03-14 23:34:43 -0400

    Log meta description contents

--- tests/scrape.spec.ts
+++ tests/scrape.spec.ts
@@ -1,64 +1,81 @@
-import { test, Page } from '@playwright/test';
+import { test, Page } from '@playwright/test'
 
 type StackReport = {
-    url: URL;
-    visited: boolean;
-    statusCode?: number;
-};
-
+    url: string | URL,
+    visited: boolean
+    statusCode?: number
+    meta?: {
+        description?: string
+        keywords?: string
+    }
+}
 type URLStack = {
-    [key: string]: StackReport;
-};
+    [key: string]: StackReport
+}
 
-const baseURL = new URL('https://www.prettygoodping.com/');
-const history: URLStack = {};
+const baseURL = new URL('https://www.prettygoodping.com/')
+const history: URLStack = {}
 
 test('has title', async ({ page }) => {
-    test.setTimeout(120000);
-    const begin = new URL(baseURL);
-    await extractLinks(begin, page);
-    console.dir(history, { depth: 3 });
-});
+    test.setTimeout(120000)
+    const begin = new URL(baseURL)
+    await extractLinks(begin, page)
+    console.dir(history, { depth: null })
+})
 
 const extractLinks = async (nextPage: URL, page: Page) => {
-    console.log(nextPage.toString());
-    const response = await page.goto(nextPage.toString(), { timeout: 10000 });
-    history[nextPage.toString()] = { url: nextPage, visited: true, statusCode: response?.status() } as StackReport;
+    console.log(nextPage.toString())
+    const response = await page.goto(nextPage.toString(), { timeout: 10000 })
+    history[nextPage.toString()] = {
+        url: nextPage.toString(),
+        visited: true,
+        statusCode: response?.status(),
+        meta: {
+            description: await page.locator('meta[name="description"]').getAttribute('content'),
+            // keywords: await page.locator('meta[name="keywords"]').innerText(),
+        },
+    } as StackReport
     if (response?.status() === 404) {
-        console.error(`404 on ${nextPage.toString()}`);
+        console.error(`404 on ${nextPage.toString()}`)
     }
 
-    const found = page.getByRole('link');
-    const count = await found.count();
+    const found = page.getByRole('link')
+    const count = await found.count()
 
     for (let i = 0; i < count; i++) {
-        const link = await found.nth(i).getAttribute('href');
+        const link = await found.nth(i).getAttribute('href')
         if (link === null) {
-            continue;
+            continue
         }
         try {
-            const url = new URL(link, baseURL);
+            const url = new URL(link, baseURL)
 
-            url.hash = ''; // ignore fragments for now
+            url.hash = '' // ignore fragments for now
             if (url.protocol === 'mailto:') {
-                continue;
+                continue
             }
-            if (url.host !== nextPage.hostname || link === '#' || url.toString() in history) {
-                continue;
-            }
-            else {
-                console.log(`${link} ${i} of ${count} ${url.toString()}`);
-                history[url.toString()] = { url: url, visited: false } as StackReport;
+            if (
+                url.host !== nextPage.hostname ||
+                link === '#' ||
+                url.toString() in history
+            ) {
+                continue
+            } else {
+                console.log(`${link} ${i} of ${count} ${url.toString()}`)
+                history[url.toString()] = {
+                    url: url,
+                    visited: false,
+                } as StackReport
             }
         } catch (error) {
-            console.log(`error: ${link} "${error}"`);
+            console.log(`error: ${link} "${error}"`)
         }
     }
     for (const key in history) {
         if (history[key].visited === false) {
-            console.log(`----- extract from ${history[key].url.toString()}`);
-            await extractLinks(history[key].url, page);
+            console.log(`----- extract from ${history[key].url.toString()}`)
+            await extractLinks(history[key].url, page)
         }
-    };
-    console.log('finished extractLinks');
-};
+    }
+    console.log('finished extractLinks')
+}
