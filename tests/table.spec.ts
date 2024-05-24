b08005134dfcfcf193ea63469834deca4690bdf0
Olaf Alders <olaf@wundersolutions.com>
2024-05-24 17:16:45 -0400

    Return all matching rows in table

--- tests/table.spec.ts
+++ tests/table.spec.ts
@@ -27,43 +27,61 @@ test('tableToLocArray', async ({ page }) => {
 
 test('find River via string match', async ({ page }) => {
     const found = page.getByRole('table');
-    const match = await rowMatch(found, 'River');
-    expect(await match[0].innerHTML()).toEqual('River');
-    expect(await match[1].innerHTML()).toEqual('Cartwright');
+    const matches = await toString(await rowMatches(found, 'River'));
+    expect(matches).toStrictEqual([['River', 'Cartwright']]);
 });
 
 test('find River via regex', async ({ page }) => {
     const found = page.getByRole('table');
-    const match = await rowMatch(found, /River/);
-    expect(await match[0].innerHTML()).toEqual('River');
-    expect(await match[1].innerHTML()).toEqual('Cartwright');
+    const matches = await toString(await rowMatches(found, /River/));
+    expect(matches).toStrictEqual([['River', 'Cartwright']]);
 });
 
 test('find multiple rows via regex', async ({ page }) => {
     const found = page.getByRole('table');
-    const match = await rowMatch(found, /am/);
-    expect(await match[0].innerHTML()).toEqual('First Name');
-    expect(await match[1].innerHTML()).toEqual('Last Name');
+    const matches = await toString(await rowMatches(found, /am/));
+    expect(matches).toStrictEqual([
+        ['First Name', 'Last Name'],
+        ['Jackson', 'Lamb'],
+    ]);
 });
 
-async function rowMatch(
+const toString = async function(locArray: locObj) {
+    let asObj: tableObj = [];
+    for (let i = 0; i < locArray.length; i++) {
+        const thisRow = locArray[i];
+        const newRow: string[] = [];
+        for (let j = 0; j < thisRow.length; j++) {
+            newRow.push(await thisRow[j].innerHTML());
+        }
+        asObj.push(newRow);
+    }
+    return asObj;
+};
+
+const rowMatches = async function(
     table: Locator,
     value: string | RegExp,
-): Promise<Locator[]> {
+): Promise<locObj> {
     const locTable = await tableToLocArray(table);
-    const allRows = await Promise.all(
-        locTable.map(async (row) => {
-            const innerHTML = await row[0].innerHTML();
-            if (typeof value === 'string') {
-                return innerHTML === value ? row : null;
-            } else {
-                return value.test(innerHTML) ? row : null;
-            }
-        }),
-    );
-    const rowWithMatchingValue = allRows.find((row) => row !== null);
-    return rowWithMatchingValue;
-}
+    const matchingRows = [];
+    for (const row of locTable) {
+        const isMatch = await Promise.all(
+            row.map(async (cell) => {
+                const innerHTML = await cell.innerHTML();
+                if (typeof value === 'string') {
+                    return innerHTML === value;
+                } else {
+                    return value.test(innerHTML);
+                }
+            })
+        );
+        if (isMatch.some(Boolean)) {
+            matchingRows.push(row);
+        }
+    }
+    return matchingRows;
+};
 
 const tableToArray = async function(table: Locator): Promise<tableObj> {
     let locTable = await tableToLocArray(table);
